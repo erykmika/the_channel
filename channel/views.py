e96054ba4ce61e4dec9607bf68b080b1a220ca7c
Eryk Mika <erykmika108@gmail.com>
2025-02-16 11:07:05 +0100

    Fix reverse lazy call in SignUp view class

--- channel/views.py
+++ channel/views.py
@@ -16,7 +16,7 @@ class SignInView(LoginView):
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("signin")
+    success_url = reverse_lazy("login")
     template_name = "signup.html"
 
 
